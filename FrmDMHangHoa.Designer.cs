cb0f04ea928fef5ef484e21e76b665137f7de547
trankieutrang <trangkieu412@gmail.com>
2020-04-01 15:27:35 +0700

    quan ly ban hang

---
+++ FrmDMHangHoa.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace demo
+{
+    partial class FrmDMHangHoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmahang = new System.Windows.Forms.TextBox();
+            this.txttenhang = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGiaNhap = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGiaBan = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtsoluong = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView_DMhang = new System.Windows.Forms.DataGridView();
+            this.MaHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GiaNhap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GiaBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnXoa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DMhang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Hàng";
+            // 
+            // txtmahang
+            // 
+            this.txtmahang.Location = new System.Drawing.Point(109, 12);
+            this.txtmahang.Name = "txtmahang";
+            this.txtmahang.Size = new System.Drawing.Size(98, 20);
+            this.txtmahang.TabIndex = 1;
+            // 
+            // txttenhang
+            // 
+            this.txttenhang.Location = new System.Drawing.Point(109, 68);
+            this.txttenhang.Name = "txttenhang";
+            this.txttenhang.Size = new System.Drawing.Size(98, 20);
+            this.txttenhang.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên Hàng";
+            // 
+            // txtGiaNhap
+            // 
+            this.txtGiaNhap.Location = new System.Drawing.Point(407, 12);
+            this.txtGiaNhap.Name = "txtGiaNhap";
+            this.txtGiaNhap.Size = new System.Drawing.Size(100, 20);
+            this.txtGiaNhap.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Giá Nhập";
+            // 
+            // txtGiaBan
+            // 
+            this.txtGiaBan.Location = new System.Drawing.Point(407, 68);
+            this.txtGiaBan.Name = "txtGiaBan";
+            this.txtGiaBan.Size = new System.Drawing.Size(100, 20);
+            this.txtGiaBan.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(320, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Giá Bán";
+            // 
+            // txtsoluong
+            // 
+            this.txtsoluong.Location = new System.Drawing.Point(110, 125);
+            this.txtsoluong.Name = "txtsoluong";
+            this.txtsoluong.Size = new System.Drawing.Size(94, 20);
+            this.txtsoluong.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "số lượng";
+            // 
+            // dataGridView_DMhang
+            // 
+            this.dataGridView_DMhang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_DMhang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaHang,
+            this.TenHang,
+            this.GiaNhap,
+            this.GiaBan,
+            this.SoLuong});
+            this.dataGridView_DMhang.Location = new System.Drawing.Point(30, 168);
+            this.dataGridView_DMhang.Name = "dataGridView_DMhang";
+            this.dataGridView_DMhang.Size = new System.Drawing.Size(547, 114);
+            this.dataGridView_DMhang.TabIndex = 10;
+            this.dataGridView_DMhang.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_DMhang_CellClick);
+            // 
+            // MaHang
+            // 
+            this.MaHang.DataPropertyName = "MaHang";
+            this.MaHang.HeaderText = "Mã Hàng";
+            this.MaHang.Name = "MaHang";
+            // 
+            // TenHang
+            // 
+            this.TenHang.DataPropertyName = "TenHang";
+            this.TenHang.HeaderText = "Tên Hàng";
+            this.TenHang.Name = "TenHang";
+            // 
+            // GiaNhap
+            // 
+            this.GiaNhap.DataPropertyName = "GiaNhap";
+            this.GiaNhap.HeaderText = "Giá Nhập";
+            this.GiaNhap.Name = "GiaNhap";
+            // 
+            // GiaBan
+            // 
+            this.GiaBan.DataPropertyName = "GiaBan";
+            this.GiaBan.HeaderText = "Giá Bán";
+            this.GiaBan.Name = "GiaBan";
+            // 
+            // SoLuong
+            // 
+            this.SoLuong.DataPropertyName = "soLuong";
+            this.SoLuong.HeaderText = "Số Lượng";
+            this.SoLuong.Name = "SoLuong";
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(66, 304);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(111, 29);
+            this.btnXoa.TabIndex = 11;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // FrmDMHangHoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 345);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.dataGridView_DMhang);
+            this.Controls.Add(this.txtsoluong);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtGiaBan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtGiaNhap);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txttenhang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmahang);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmDMHangHoa";
+            this.Text = "FrmDMHangHoa";
+            this.Load += new System.EventHandler(this.FrmDMHangHoa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DMhang)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmahang;
+        private System.Windows.Forms.TextBox txttenhang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGiaNhap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGiaBan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtsoluong;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView_DMhang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaHang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenHang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GiaNhap;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GiaBan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoLuong;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
---
+++ FrmDMHangHoa.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace demo
+{
+    public partial class FrmDMHangHoa : Form
+    {
+        SqlConnection con = new SqlConnection();
+        public FrmDMHangHoa()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDMHangHoa_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-UH0D2FE\\SQLEXPRESS01;Initial Catalog=Qly_Banhang;Integrated Security=True";
+            con.ConnectionString = connectionString;
+            con.Open();
+            //string sql = "select * from DMHang";
+
+            //SqlDataAdapter adp = new SqlDataAdapter(sql,con);
+            //DataTable tableDMHang = new DataTable();
+            //adp.Fill(tableDMHang);
+            //dataGridView_DMhang.DataSource = tableDMHang;
+            loadDataToGridview();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            //delete from DMHang where MaHang='001'
+            string sql = "Delete from DMHang where MaHang=' " + txtmahang.Text + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            //sql = "select * from DMHang";
+            //SqlDataAdapter adp = new SqlDataAdapter(sql, con);
+
+            //DataTable tableDMHang = new DataTable();
+            //adp.Fill(tableDMHang);
+
+            //dataGridView_DMhang.DataSource = tableDMHang;
+            loadDataToGridview();
+        }
+        private void loadDataToGridview()
+        {
+            string sql = "select * from DMHang";
+
+            SqlDataAdapter adp = new SqlDataAdapter(sql, con);
+            DataTable tableDMHang = new DataTable();
+            adp.Fill(tableDMHang);
+            dataGridView_DMhang.DataSource = tableDMHang;
+        }
+
+        private void dataGridView_DMhang_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtGiaBan.Text = dataGridView_DMhang.CurrentRow.Cells["GiaBan"].Value.ToString();
+            txtGiaNhap.Text = dataGridView_DMhang.CurrentRow.Cells["GiaNhap"].Value.ToString();
+            txtmahang.Text = dataGridView_DMhang.CurrentRow.Cells["MaHang"].Value.ToString();
+            txtsoluong.Text = dataGridView_DMhang.CurrentRow.Cells["soLuong"].Value.ToString();
+            txttenhang.Text = dataGridView_DMhang.CurrentRow.Cells["TenHang"].Value.ToString();
+
+            txtmahang.Enabled = false;
+        }
+    }
+}
